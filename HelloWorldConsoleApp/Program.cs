430ab26fb6a5d2563f671739b2414fa59c7a0ee4
Juan Esteban Angulo Uribe <jangulorama@gmail.com>
2023-08-24 21:12:12 -0500

    Add my first console app

---
+++ HelloWorldConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HelloWorldConsoleApp.Dtos;
+
+public static class SayHello
+{
+    public static void Main (){
+
+        Console.WriteLine("Hola mundo!");
+        var person = new Person();
+        person.Name = "Jhon";
+        person.Age = 18;
+
+        string message = $"Hello Mr {person.Name} your age is {person.Age}";
+        Console.WriteLine(message);
+        Console.ReadLine();
+    }
+}
+
